87dcc44d27b82e4e7267b161df8fc3a9a6f35d81
tasCode <1209540473@qq.com>
2021-04-19 09:16:18 +0800

    Queen8

---
+++ src/com/michael/Queen8Demo.java
@@ -0,0 +1,39 @@
+package com.michael;
+
+public class Queen8Demo {
+	int max = 8;
+	int[] array = new int[max];
+	static int count = 0;
+	public static void main(String[] args) {
+		Queen8Demo q = new Queen8Demo();
+		q.check(0);
+		System.out.printf("一共有%d解法",count);
+	}
+	private void check(int n) {
+		if(n==max) {
+			print();
+			return;
+		}
+		for(int i=0;i<max;i++) {
+			array[n] = i;
+			 if(judge(n)) {
+				 check(n+1);
+			 }
+		}
+	}
+	private boolean judge(int n) {
+		for(int i=0;i<n;i++) {
+			if(array[i] == array[n] || Math.abs(n-i) == Math.abs(array[n]-array[i])) {
+				return false;
+			}
+		}
+		return true;
+	}
+	private void print() {
+		count++;
+		for(int i=0;i<array.length;i++) {
+			System.out.print(array[i]+" ");
+		}
+		System.out.println();
+	}
+}
